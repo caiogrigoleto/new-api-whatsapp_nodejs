075a8f1b9a8e6d7f04053e6f7f3d790f7ba35cc8
Caio Grigoleto <62262505+caiogrigoleto@users.noreply.github.com>
2023-08-31 14:02:20 -0300

    algumas alteracoes

--- localauth.js
+++ localauth.js
@@ -17,119 +17,145 @@ require('dotenv').config();
 
 
 // mongoose.connect(process.env.MONGODB_URI).then(() => {
-  session = function (session) {
-    console.log('session', session);
+session = function (session) {
+  console.log('session', session);
 
-    const client = new Client({
-      authStrategy: new LocalAuth({
-        clientId: md5(session),
-      })
-    });
-
-    client.initialize();
-
-    client.on(`qr`, (qr) => {
-      qrcode.generate(qr, { small: true });
-    });
-
-    client.on(`authenticated`, async (session) => {
-      console.log('Authenticated');
-    });
-
-    client.on(`auth_failure`, (msg) => {
-      console.log(`auth_failure`, msg);
-    });
-
-    client.on(`ready`, () => {
-      console.log(`Ready`);
-    });
-
-    client.on(`disconnected`, (reason) => {
-      console.log(`disconnected`, reason);
-    });
-  }
-
-  const restoreSession = function (session) {
-    console.log('session', session);
-
-    const client = new Client({
-      authStrategy: new LocalAuth({
-        clientId: md5(session),
-      })
-    });
-    client.initialize();
-
-    client.on(`authenticated`, async (session) => {
-      console.log('Authenticated');
-    });
-
-    client.on(`auth_failure`, (msg) => {
-      console.log(`auth_failure`, msg);
-    });
-
-    client.on(`ready`, () => {
-      console.log(`Ready`);
-    });
-
-    client.on(`disconnected`, (reason) => {
-      console.log(`disconnected`, reason);
-    });
-
-    return client;
-  }
-
-
-  app.use(bodyParser.json());
-  app.use(cors());
-
-  const PORT = process.env.PORT || 3000;
-
-  app.listen(PORT, () => {
-    console.log(`Servidor da API iniciado na porta ${PORT}`);
-  });
-
-  app.post('/instancia/iniciar', (req, res) => {
-    const instance = req.query.instancia;
-
-    new session(instance).then(response => {
-      res.status(200).json({
-        status: true,
-        data: response
-      });
+  const client = new Client({
+    puppeteer: {
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--no-first-run",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--disable-gpu",
+        "--single-process",
+        "--no-zygote",
+      ],
+    },
+    authStrategy: new LocalAuth({
+      clientId: md5(session),
     })
   });
 
-  let sessions = {
-    //1: restoreSession('TESTE'),
-    2: restoreSession('TESTE1')
-  };
+  client.initialize();
 
-  app.post('/mensagens/enviarMensagem', (req, res) => {
-    const instance = req.query.instancia;
-    const number = req.body.numero;
-    const message = req.body.mensagem;
-
-    sessions[instance].sendMessage(number + '@c.us', message).then(response => {
-      res.status(200).json({
-        status: true,
-        data: {
-          remote: response['_data']['id']['remote'],
-          id: response['_data']['id']['id'],
-          body: response['_data']['body'],
-        },
-      });
-    }).catch(err => {
-      res.status(500).json({
-        status: false,
-        response: err
-      });
-    })
+  client.on(`qr`, (qr) => {
+    qrcode.generate(qr, { small: true });
   });
 
-  app.get('/instancia/listar', async (req, res) => {
+  client.on(`authenticated`, async (session) => {
+    console.log('Authenticated');
+  });
+
+  client.on(`auth_failure`, (msg) => {
+    console.log(`auth_failure`, msg);
+  });
+
+  client.on(`ready`, () => {
+    console.log(`Ready`);
+  });
+
+  client.on(`disconnected`, (reason) => {
+    console.log(`disconnected`, reason);
+  });
+}
+
+const restoreSession = function (session) {
+  console.log('session', session);
+
+  const client = new Client({
+    puppeteer: {
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--no-first-run",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--disable-gpu",
+        "--single-process",
+        "--no-zygote",
+      ],
+    },
+    authStrategy: new LocalAuth({
+      clientId: md5(session),
+    })
+  });
+  client.initialize();
+
+  client.on(`authenticated`, async (session) => {
+    console.log('Authenticated');
+  });
+
+  client.on(`auth_failure`, (msg) => {
+    console.log(`auth_failure`, msg);
+  });
+
+  client.on(`ready`, () => {
+    console.log(`Ready`);
+  });
+
+  client.on(`disconnected`, (reason) => {
+    console.log(`disconnected`, reason);
+  });
+
+  return client;
+}
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor da API iniciado na porta ${PORT}`);
+});
+
+app.post('/instancia/iniciar', (req, res) => {
+  const instance = req.query.instancia;
+
+  new session(instance).then(response => {
     res.status(200).json({
-      status: true
-    })
-  });
+      status: true,
+      data: response
+    });
+  })
+});
+
+let sessions = {
+  //1: restoreSession('TESTE'),
+  //2: restoreSession('TESTE1')
+};
+
+app.post('/mensagens/enviarMensagem', (req, res) => {
+  const instance = req.query.instancia;
+  const number = req.body.numero;
+  const message = req.body.mensagem;
+
+  sessions[instance].sendMessage(number + '@c.us', message).then(response => {
+    res.status(200).json({
+      status: true,
+      data: {
+        remote: response['_data']['id']['remote'],
+        id: response['_data']['id']['id'],
+        body: response['_data']['body'],
+      },
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  })
+});
+
+app.get('/instancia/listar', async (req, res) => {
+  res.status(200).json({
+    status: true
+  })
+});
 // });
 // 
